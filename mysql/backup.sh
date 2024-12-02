da8a0a5cf6038b9be8fe24610ca946a80294e5f1
杨亚梦 <mz.yyam@gmail.com>
2024-12-02 13:44:01 +0800

    %H -> %-H
    小于10点，%H 会有个空格导致无法还原

--- mysql/backup.sh
+++ mysql/backup.sh
@@ -4,7 +4,7 @@
 backup_dir="/app_backup"
 
 # 创建备份文件名，格式为：db_name-年-月-日_时-分-秒.sql.gz
-backup_file="$backup_dir/$BACKUP_MYSQL_DB-$(date +%F_%H-%M-%S).sql.gz"
+backup_file="$backup_dir/$BACKUP_MYSQL_DB-$(date +%F_%-H-%M-%S).sql.gz"
 
 # 备份MySQL数据库
 mysqldump -u "$BACKUP_MYSQL_USERNAME"\
